eef4e2fda94d4470e8d2c00923c16cfde2ee60a2
markojov0410 <markojovfreelancer@gmail.com>
2022-06-20 21:26:24 +0200

    'init'

---
+++ src/exercise/03.js
@@ -0,0 +1,72 @@
+// Flexible Compound Components
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+// 🐨 create your ToggleContext context here
+// 📜 https://reactjs.org/docs/context.html#reactcreatecontext
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
+}
+
+// 🐨 we'll still get the children from props (as it's passed to us by the
+// developers using our component), but we'll get `on` implicitly from
+// ToggleContext now
+// 🦉 You can create a helper method to retrieve the context here. Thanks to that,
+// your context won't be exposed to the user
+// 💰 `const context = React.useContext(ToggleContext)`
+// 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
+function useToggle() {
+  return React.useContext(ToggleContext)
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
+  console.log('ToggleOn state', on)
+  return on ? children : null
+}
+
+// 🐨 do the same thing to this that you did to the ToggleOn component
+function ToggleOff({children}) {
+  const {on} = useToggle()
+  console.log('ToggleOff state', on)
+  return on ? null : children
+}
+
+// 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
+function ToggleButton(...props) {
+  const {on, toggle} = React.useContext(ToggleContext)
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <div>
+          <ToggleButton />
+        </div>
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
---
+++ src/exercise/04.js
@@ -0,0 +1,56 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+const CallAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  // 🐨 Add a property called `togglerProps`. It should be an object that has
+  // `aria-pressed` and `onClick` properties.
+  // 💰 {'aria-pressed': on, onClick: toggle}
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: CallAll(onClick, toggle),
+      ...props,
+    }
+  }
+  return {
+    on,
+    toggle,
+    getTogglerProps,
+  }
+  //return {on, toggle}
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+
+  return (
+    <div>
+      <Switch {...getTogglerProps({on})} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.log('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
